e6465faf3648a09590a5b456637a5aefff286d54
26june <gj.abordo1@gmail.com>
2023-10-03 10:14:31 +0100

    add more items

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//Image Area
+const imgSection = document.getElementById("imgSection");
+
 // Image tags
 const leftImage = document.getElementById("left");
 const midImage = document.getElementById("mid");
@@ -13,6 +16,22 @@ const allProducts = [
   new newProduct("bag", "./assets/bag.jpg"),
   new newProduct("banana", "./assets/banana.jpg"),
   new newProduct("bathroom", "./assets/bathroom.jpg"),
+  new newProduct("boots", "./assets/boots.jpg"),
+  new newProduct("breakfast", "./assets/breakfast.jpg"),
+  new newProduct("bubblegum", "./assets/bubblegum.jpg"),
+  new newProduct("chair", "./assets/chair.jpg"),
+  new newProduct("cthulu", "./assets/cthulhu.jpg"),
+  new newProduct("dog-duck", "./assets/dog-duck.jpg"),
+  new newProduct("dragon", "./assets/dragon.jpg"),
+  new newProduct("pen", "./assets/pen.jpg"),
+  new newProduct("pet-sweep", "./assets/pet-sweep.jpg"),
+  new newProduct("scissors", "./assets/scissors.jpg"),
+  new newProduct("shark", "./assets/shark.jpg"),
+  new newProduct("sweep", "./assets/sweep.png"),
+  new newProduct("tauntaun", "./assets/tauntaun.jpg"),
+  new newProduct("unicorn", "./assets/unicorn.jpg"),
+  new newProduct("water-can", "./assets/water-can.jpg"),
+  new newProduct("wine-glass", "./assets/wine-glass.jpg"),
 ];
 
 function generateIndex() {
@@ -40,9 +59,16 @@ function displayProduct() {
 
 displayProduct();
 
-gridArea.addEventListener("click", handleProductClick);
+imgSection.addEventListener("click", handleProductClick);
 
-function handleProductClick(event) {
-  console.log(event);
-  alert("Clicked");
+function handleProductClick({ target }) {
+  if (target === leftImage) {
+    console.log(leftImage.alt);
+  } else if (target === midImage) {
+    console.log("Clicked Mid");
+  } else if (target === rightImage) {
+    console.log("Clicked Right");
+  } else {
+    console.log("Clicked on the container");
+  }
 }
